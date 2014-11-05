bc2d49ecd666deb36fe2012475b61ebfde599adc
sp00f <e.cudjoe@hotmail.co.uk>
2014-11-05 12:14:58 +0000

    added choice of output

--- totalfilesize.py
+++ totalfilesize.py
@@ -14,26 +14,35 @@
 import re
 import sys
 
-#def Main():
-file = sys.argv[1]
-if file.endswith('.txt'):
-	data = open(sys.argv[1]).read()
+def Main():
+	file = sys.argv[1]
+	if file.endswith('.txt'):
+		data = open(sys.argv[1]).read()
 
-	# Separate MB values from GB values
-	m = re.findall(r'\t(\d*.\d*)\sMB\r', data)
-	g = re.findall(r'\t(\d*.\d*)\sGB\r', data)
+		# Separate MB values from GB values
+		megb = re.findall(r'\t(\d*.\d*)\sMB\r', data)
+		gigb = re.findall(r'\t(\d*.\d*)\sGB\r', data)
 
-	mega = [float(item) for item in m]
-	giga = [float(item) for item in g]
+		mg = [float(m) for m in megb]
+		gg = [float(g) for g in gigb]
 
-	totalgigs = sum(mega) / 1000 + sum(giga)
-	tera = totalgigs / 1000
+	#	Add to separate collections	
+	#	mg, gg = zip(*[(float(mg), float(gg)) for mg, gg in zip(megb, gigb)])
 
-	print('total GB: {0:,.2f}GB'.format(totalgigs))
-	print('total TB: {0:.2f}TB'.format(tera))
-	start = False
-else:
-	print('Please use a \'.txt\' file.')
+		totalgigs = sum(mg) / 1000 + sum(gg)
+		tera = totalgigs / 1000
+
+		# Select output in Terabytes or Gigabytes
+		output = raw_input(
+			'Select \'T\' for terabytes or \'G\' for gigabytes:\n').lower()
+		if output == 't':
+			print('Total TB: {0:.2f}TB'.format(tera))
+		elif output == 'g':
+			print('Total GB: {0:,.2f}GB'.format(totalgigs))
+		else:
+			print('Total TB: {0:.2f}TB'.format(tera))
+	else:
+		print('Please use a \'.txt\' file.')
 		
-#if __name__ == '__main__':
-#	Main()
+if __name__ == '__main__':
+	Main()

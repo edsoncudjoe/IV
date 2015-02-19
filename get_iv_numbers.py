d4b2ef4087eccfd6b638bdb2bee4725764169fd5
sp00f <e.cudjoe@hotmail.co.uk>
2015-02-19 19:57:05 +0000

    specified on the Power catalogue.

--- get_iv_numbers.py
+++ get_iv_numbers.py
@@ -18,32 +18,53 @@ catalogs = requests.get(url + "/catalogs;jsessionid=" + sessionid)
 
 
 catalog_data = json.loads(catalogs.text)
+
+
 catalog_id = [(i['groupName'], i['ID']) for i in catalog_data['data'] if i['ID'] > 1]
 
-def get_IV_numbers(client):
+#def get_IV_numbers(client):
 	# Get Content Media IV numbers
-	content_raw = requests.get(url + '/clips;jsessionid=' + sessionid + '?filter=and((catalog.id)eq({}))&include=userFields'.format(client))
+content_raw = requests.get(url + '/clips;jsessionid=' + sessionid + '?filter=and((catalog.id)eq({}))&include=userFields'.format(catalog_id[4][1]))
 	# REECEIVNG POSSIBLE SERVER ERROR AT THIS POINT. 
-	content_data = json.loads(content_raw.text)
+content_data = json.loads(content_raw.text)
 
 	#if there are any keys called userFields and there is a U7 in those keys, add the value of the U7 key to a list. 
-	content_iv_no = [i['userFields']['U7'] for i in content_data['data']['items'] if 'userFields' in i.keys() if 'U7' in i['userFields']]
-	final = sorted(set(content_iv_no))
+#content_iv_no = [i['userFields']['U7'] for i in content_data['data']['items'] if 'userFields' in i.keys() if 'U7' in i['userFields']]
+#content_iv_no = [i['userFields']['U7'] for i in content_data['data']['items']]
+def power_iv_gen():
+	count = 0
+
+	try:
+		for i in content_data['data']['items']:
+			if 'userFields' in i.keys():
+				if 'U7' in i['userFields']:
+					count += 1
+					print 'Position: ' + count
+					yield i['userFields']['U7']
+	except:
+		print('Error at position: {}'.format(count))
+
+
+pow_iv = power_iv_gen()
+
+#for i in range(11):
+content_iv_no = [next(pow_iv) for i in range(len(content_data['data']['items']))]	
+final = sorted(set(content_iv_no))
 	
-	return final
+#	return final
 
 
-choice = raw_input('name: ')
-if choice == 'content':	
+#choice = raw_input('name: ')
+#if choice == 'content':	
 	# Content Media
-	content = get_IV_numbers(catalog_id[1][1])
-elif choice == 'classic':
+#	content = get_IV_numbers(catalog_id[1][1])
+#elif choice == 'classic':
 	# Classic Media
-	classic = get_IV_numbers(catalog_id[0][1])
-elif choice == 'ng':
+#	classic = get_IV_numbers(catalog_id[0][1])
+#elif choice == 'ng':
 	# NGTV
-	ngtv = get_IV_numbers(catalog_id[2][1])
-	ngtv.append(get_IV_numbers(catalog_id[3][1]))
-elif choice == 'power':
+#	ngtv = get_IV_numbers(catalog_id[2][1])
+#	ngtv.append(get_IV_numbers(catalog_id[3][1]))
+#elif choice == 'power':
 	# Power
-	power = get_IV_numbers(catalog_id[4][1])
+#	power = get_IV_numbers(catalog_id[4][1])
